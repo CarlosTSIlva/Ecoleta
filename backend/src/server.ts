4f426382db3f91fe7e954c1f73964f829d443114
CarlosTeixeira615 <carlosoteixeira.tc@hotmail.com>
2020-09-10 15:53:07 -0300

    inicio

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,29 @@ import express from 'express';
 
 const app = express();
 
+const users =['joao','fernandso', 'maikon','fernandso', 'maikon']
+
 app.get('/users',(req, res) => {
-    console.log('listagem de usuarios');
-    res.json(['fernandso', 'maikon']);
+    const search = String(req.query.search)
+    const filteredUsers =search ? users.filter(user => user.includes(search)) : users
+    return res.json(filteredUsers);
 })
 
-app.listen(3333)
+app.get('/users/:id', (req,res) => {
+    const id = Number(req.params.id);
+
+    const user = users[id]
+    return res.json(user)
+})
+
+app.post('/users', (req, res) => {
+    const user= {
+        name : 'carlos eduardo',
+        email : 'carlos@hotmail.com'
+    }
+    return res.json(user)
+})
+
+app.listen(3333, () => {
+    console.log("💕 server on na porta 3333")
+})
